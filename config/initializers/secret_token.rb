85a3de8af195d5a530a99b6030a296468fd98add
Conan Rimmer <conan@Conans-MacBook-Pro.local>
2011-12-15 19:01:21 -0600

    initial drop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e611cb28cb2d69fb54af56e1faaab4db87fe521dc87df59611626c8ced61d4f89943229d46c948e59f11661de5a19fddb558339cdc44b8121e5150da1759a6ef'
